6b56c699d217f0073aa8218002951adac9a58dea
chenin <1185288254@qq.com>
2020-08-05 09:52:16 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="attr">
+    <div>hello world</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      attr:''
+    }
+  },
+  mounted() {
+    addEventListener('resize', this.onChangeSize);
+    this.onChangeSize();
+
+  },
+  methods:{
+    onChangeSize() {
+      var widthScreen = document.documentElement.clientWidth || document.body.clientWidth;
+      if(widthScreen > 900 ) {
+          this.attr = 'pc-view';
+      } else {
+        this.attr = 'moblie-view';
+      }
+    }
+  },
+  destroyed() {
+    removeEventListener('resize',this.onChangeSize);
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 25px;
 }
 </style>
